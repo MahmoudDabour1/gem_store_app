5b55dc874b12e0e62a68b644bd9ba936fefaba26
MahmoudDabour1 <mahmoud.dabour1000000@gmail.com>
2025-02-07 17:52:02 +0200

    Initial commit

---
+++ lib/core/routing/app_router.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:gem_store_app/core/routing/routes.dart';
+import 'package:gem_store_app/features/onboarding/onboarding_screen.dart';
+
+class AppRouter {
+  Route? generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.onBoardingScreen:
+        return MaterialPageRoute(
+          builder: (_) =>  OnboardingScreen(),
+        );
+      default:
+        return null;
+    }
+  }
+}
