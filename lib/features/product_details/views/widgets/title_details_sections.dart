5dadfe189e50bda2bd6dbcb8f50e01e97b639965
Norhan Helal <norhanhelal678@gmail.com>
2025-02-13 22:10:59 +0200

    wishlist_boards  done

--- lib/features/product_details/views/widgets/title_details_sections.dart
+++ lib/features/product_details/views/widgets/title_details_sections.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:gem_store_app/core/utils/app_colors.dart';
