69e6647429cf5651844aca0a9b2b5f727dfd9ff5
ABDO-OS <abduos369@gmail.com>
2025-03-20 22:23:01 +0200

    Add additional project files and remove unused assets across platforms

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,7 +1,7 @@
-
 import 'package:device_preview/device_preview.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -20,11 +20,10 @@ import 'gem_store_app.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
-
   WidgetsFlutterBinding.ensureInitialized();
-  // FlutterError.onError = (FlutterErrorDetails details) {
-  //   FirebaseCrashlytics.instance.recordFlutterFatalError(details);
-  // };
+  FlutterError.onError = (FlutterErrorDetails details) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(details);
+  };
 
   await ScreenUtil.ensureScreenSize();
   await setupGetIt();
